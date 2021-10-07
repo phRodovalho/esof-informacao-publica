24db5301420cb23f1e345abe29616db0c46bae67
Phelipe S. Rodovalho <59853753+phRodovalho@users.noreply.github.com>
2021-10-07 16:29:47 -0300

    Update new-post.php

--- view/new-post.php
+++ view/new-post.php
@@ -1,16 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-
+<head>    
     <link rel="stylesheet" type="text/css" href="style-blog.css" />
-    <title>Public-information</title>
 </head>
 
 <body>
